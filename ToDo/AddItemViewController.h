925769ee6200bafe0284420f7adcb8595bbdf1ba
IshaGupta18 <ishagupta1828@gmail.com>
2021-05-23 23:09:48 +0530

    feat: add controller for adding a todo item

--- ToDo/AddItemViewController.h
+++ ToDo/AddItemViewController.h
@@ -9,7 +9,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@class AddItemViewController;
+
+@protocol AddItemViewControllerDelegate <NSObject, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource>
+
+- (void)sendItemToAdd: (AddItemViewController *) controller title: (NSString*) itemTitle priority: (NSString*) itemPriority time: (NSString*) timeToComplete;
+
+@end
+
 @interface AddItemViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UITextField *ItemTitle;
+@property (strong, nonatomic) IBOutlet UITextField *ItemTimeToComplete;
+@property (strong, nonatomic) IBOutlet UIButton *AddItem;
+@property (strong, nonatomic) IBOutlet UIPickerView *ItemPriorityPicker;
+@property (weak, nonatomic) id <AddItemViewControllerDelegate> delegate;
+- (IBAction)addIteamToList:(id)sender;
 
 @end
 
--- ToDo/AddItemViewController.m
+++ ToDo/AddItemViewController.m
@@ -8,24 +8,47 @@
 #import "AddItemViewController.h"
 
 @interface AddItemViewController ()
-
+@property NSArray<NSString*> *priorityValues;
+@property NSString* priorityValue;
 @end
 
 @implementation AddItemViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.ItemTitle.delegate = self;
+    self.ItemPriorityPicker.delegate = self;
+    self.ItemPriorityPicker.dataSource = self;
+    self.priorityValues = @[@"None", @"High", @"Medium", @"Low"];
+    self.priorityValue = @"None";
+}
+
+- (NSInteger) numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 1;
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+ On touch of Done button, transfer the data to the ViewController and dismiss the current scene.
+ */
+- (IBAction)addIteamToList:(id)sender {
+    [self.delegate sendItemToAdd:self title:self.ItemTitle.text priority:self.priorityValue time: self.ItemTimeToComplete.text];
+    [[self presentingViewController] dismissViewControllerAnimated:YES completion:nil];
 }
-*/
 
+- (NSInteger) pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    return self.priorityValues.count;
+}
+
+- (NSString*) pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
+    return self.priorityValues[row];
+}
+
+- (void) pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    self.priorityValue = [self.priorityValues objectAtIndex:row];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    [textField resignFirstResponder];
+    return YES;
+}
 @end
